a7865e1a8a830a36b276785bd188897d1cca7480
kuzziiX-dotcom <pahasaramathish295@gmail.com>
2026-01-24 15:33:34 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker Placeholder
+self.addEventListener('install', (e) => {
+  console.log('Lochi App Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // මේක හිස්ව තිබුනට කමක් නැහැ දැනට
+});
